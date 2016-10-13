87bea7ce969ec479499ca86598928a0d6d302425
Damian Mandola Jr <damianjr@Damian-Mandola-Jrs-MacBook-Pro.local>
2016-10-13 17:50:01 -0500

    added require variables and started twitter function

--- liri.js
+++ liri.js
@@ -1,21 +1,31 @@
 // grab the keys from the key.js file
 var keys = require('./keys.js');
+//use require to use npm packages
+var Twitter = require('twitter');
+var Spotify = require('spotify');
+// putting the twitter keys in a variable to use in the tweets function
+var client = new Twitter(keys.twitterKeys);
+var fs = require('fs');
+//var spotKeys = keys.spotifyKeys;
 
-// get all the various keys form the file
-var twitterKeys = keys.twitterKeys;
-var spotifyKeys = keys.spotifyKeys;
+console.log(keys.twitterKeys);
 
-// loop through the keys and print them (just to check if they're working, this wouldn't be secure otherwise)
-
-for (var key in twitterKeys) {
-	console.log("Twitter key: " + twitterKeys[key]);
+function twitter(){
+	var params = {screen_name: 'damdola'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  	if (!error) {
+    console.log(tweets);
+  }
+});
 };
 
-for (var key in spotifyKeys) {
-	console.log("Spotify key: " + spotifyKeys[key]);
-};
 
-// if (process.argv[2] == "my-tweets"){
-	
+// if (process.argv[2] == "my-tweets") {
+
+ 
+// };
+
+
+
 
 // } else if()
